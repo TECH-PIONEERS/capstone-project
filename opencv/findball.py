a74a9974a5142428adba515c431b61ce76d9343a
swanim <64563859+swanim@users.noreply.github.com>
2024-03-27 18:11:17 +0900

    add : findball 주석 추가

--- opencv/findball.py
+++ opencv/findball.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -7,7 +6,6 @@ import cv2
 import imutils
 import time
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
     help="path to the (optional) video file")
@@ -15,9 +13,8 @@ ap.add_argument("-b", "--buffer", type=int, default=64,
     help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
+
+# 노랑색을 검출하기 위한 상한값, 하한값 경계 정의
 #colorLower = (0, 100, 100)  
 colorLower = (0, 138, 138)  
 colorUpper = (40, 255, 255) 
@@ -26,48 +23,40 @@ pts = deque(maxlen=args["buffer"])
     
 vs = cv2.VideoCapture(0)
 
-# allow the camera or video file to warm up
+# 카메라나 비디오 파일을 시작하기 위한 중단 시간 부여
 time.sleep(2.0)
 
-# keep looping
 while True:
-    # grab the current frame
+    # 현재 프레임 잡기
     _, frame = vs.read()
-    # handle the frame from VideoCapture or VideoStream
+
+    # VideoCapture, VideoStream 프레임 조정
     # frame = frame[1] if args.get("video", False) else frame
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
+    # 프레임을 잡지 못하면 비디오 종료
     if frame is None:
         break
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
+    # 프레임 크기 조정, 블러 처리, HSV 색 공간으로 변환
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    #print("**" + frame)
-
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
+    
+    # 노랑색에 대한 마스크 생성 후, 마스크 내의 deliations과 erotions를 제겅
     mask = cv2.inRange(hsv, colorLower, colorUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
+    # 마스크 내의 윤곽선 찾기 및 현재 (x, y) 초기화
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     #cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     center = None
 
-    # only proceed if at least one contour was found
+    # 하나의 윤곽선이 발견된 경우
     if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
+        # 마스크 내에서 가장 큰 윤곽선 찾은 후, 최소 경계 원 및 중심 계산
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
@@ -78,6 +67,7 @@ while True:
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             print(center)
+            
     pts.appendleft(center)
 
     for i in range(1, len(pts)):
@@ -87,14 +77,13 @@ while True:
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
     
-    # show the frame to our screen
+    # 프레임 보여주기
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the 'q' key is pressed, stop the loop
+    # 'q'키를 눌러 while문 종료
     if key == ord("q"):
         break
 
-
-# close all windows
+# 모든 윈도우 종료
 cv2.destroyAllWindows()
