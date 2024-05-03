3848650c5c37d1c9bc0443f47409b1f492a643c5
swanim <stellarswan20@gmail.com>
2024-05-03 12:35:08 +0100

    add : ir_result.txt

--- IR/pyserial_test.py
+++ IR/pyserial_test.py
@@ -4,17 +4,21 @@ import time
 
 #ser = serial.Serial('/dev/ttyAMA10',19200)
 ser = serial.Serial('/dev/ttyUSB0',19200)
-print(ser)
+ser1 = serial.Serial('/dev/ttyUSB1',19200)
+#print(ser)
 time.sleep(2)  
-i=0
+
 #try:
 while True:
-    print(i)
-    data = ser.readline()
-    print(data)
-        # if ser.in_waiting > 0:
-        #     data = ser.readline().decode().strip()
-        #     print("Received:", data)
+    # data = ser.readline()
+    # data1 = ser1.readline()
+    # print(data)
+    if ser.in_waiting > 0 and ser1.in_waiting > 0:
+        data = ser.readline().strip()
+        data1 = ser1.readline().strip()
+        print("data 0 Received:", data)
+        print("data 1 Received:", data1)
+    time.sleep(2)  
 # except KeyboardInterrupt:
 #     print("Keyboard Interrupt detected. Exiting...")
 # finally:
