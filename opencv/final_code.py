3ec3427fb7790d8cc5caa39a84488c6301827c97
Ryu jisu <wltn1418@gmail.com>
2024-05-20 18:45:45 +0900

    feat: tts & jisu code 합침

---
+++ opencv/final_code.py
@@ -0,0 +1,251 @@
+from multiprocessing import Process, Pipe, Manager
+from picamera2 import Picamera2
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+import utils 
+import threading 
+import serial
+import cv2
+import numpy as np
+import time
+
+lf = b'\n'  # Linefeed in ASCII
+myString = None
+myString1 = None
+golfball_size = 3
+flag = 0
+start_x, start_y, end_x, end_y, goal_x, goal_y = 0, 0, 0, 0, 0, 0
+previous_frame = None
+
+global previous_x
+global previous_y
+previous_x = -999
+previous_y = -999
+# colorLower = (0, 138, 138) # tuning required! 
+colorLower = (5, 152, 152) 
+colorUpper = (150, 250, 250) 
+# colorLower = ( 0, 0, 200) # setting for red
+# colorUpper = ( 60, 10, 255) # GBR
+
+previous_direction = ''
+
+# 원이 범위를 벗어나면 경고 문구 출력하는 함수
+def ballOutOfRangeAlert(circles, SCREEN_WIDTH):
+    # 경계 범위 설정
+    BOUNDARY_X = SCREEN_WIDTH // 3 # 화면 너비의 1/3
+    for (x, y, r) in circles:
+        if x - r < BOUNDARY_X:
+            print("Circle is going out of screen boundary!")
+            if not utils.is_beeping:
+                beep_thread = threading.Thread(target=utils.generate_alert_beep)
+                beep_thread.start()
+                print("Running alert beep")
+            if not utils.isBallOutOfRange:
+                utils.isBallOutOfRange = True
+        elif utils.isBallOutOfRange is True:
+            utils.isBallOutOfRange = False
+
+def get_position(event, x, y, flags, params):
+    global start_x 
+    global start_y 
+    global end_x 
+    global end_y 
+    global flag
+    global goal_x
+    global goal_y
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Clicked at (x={}, y={})".format(x, y))
+        if flag == 0:
+            start_x = x
+            start_y = y
+            flag = 1
+        elif flag == 1:
+            end_x = x
+            end_y = y
+            flag = 2  # flag를 2로 설정하여 crop할 좌표를 모두 선택한 상태로 변경
+            utils.pixel_to_cm(end_y-start_y)
+            # cv2.destroyWindow('cap')  # 마우스 클릭 이벤트를 위한 창 닫기
+        elif flag == 2:
+            goal_x = x
+            goal_y = y
+            flag = 3
+    return 
+
+def tts_process(shared_value):
+    while True:
+        tts_flag = shared_value.value
+        print(tts_flag)
+
+        if utils.is_beeping is False:
+            if tts_flag == 999: #퍼터 값이 없을 경우
+                print("Running alert beep")
+                beep_thread = threading.Thread(target=utils.generate_alert_beep)
+                beep_thread.start()
+                beep_thread.join()
+
+def stream_opencv(conn, ball_position):
+    global previous_direction
+    global previous_x
+    global previous_y
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video",
+        help="path to the (optional) video file")
+    ap.add_argument("-b", "--buffer", type=int, default=64,
+        help="max buffer size")
+    args = vars(ap.parse_args())
+
+    pts = deque(maxlen=args["buffer"])
+    picam2 = Picamera2()
+    picam2.video_configuration.controls.FrameRate = 60.0
+    picam2.preview_configuration.main.size=(640, 480)
+    picam2.preview_configuration.main.format = "RGB888"
+    picam2.start()
+
+    while True:
+
+        cap = picam2.capture_array()
+        if cap is None:
+            print('no frame')
+            cap = previous_frame  # 이전 프레임을 사용
+        else:
+            previous_frame = cap
+
+        cap = utils.camera_calibration(cap)
+        cv2.namedWindow('cap')
+        cv2.setMouseCallback('cap', get_position)
+
+        if flag == 3:
+            frame = cap[start_y:end_y, start_x:end_x]
+            SCREEN_WIDTH = end_x - start_x
+            SCREEN_HEIGHT = end_y - start_y
+
+            if conn.poll():
+                res = conn.recv()
+                output = res[0]
+                output1 = res[1]
+                # print(f'{output} {output1}')
+                if(len(output1) > 0):
+                    # print(output1[0]//4 -80)
+                    if output1[0]//4 -80 <= -80:
+                        continue
+                    elif output1[0]//4 -80 <= 15:
+                        calibration = 0.43
+                    elif output1[0]//4 -80 <= 60:
+                        calibration = 0.42
+                    else: 
+                        calibration = 0.41
+                    if(len(output) > 1):
+                        cv2.circle(frame,( int(output1[0]//4 * calibration), int((output[0])//4 * 0.4)), 5, (0,0,255), -1)
+                    if(len(output) > 3):
+                        cv2.circle(frame,( int(output1[0]//4 * calibration), int((output[2])//4 * 0.4)), 5, (255,0,255), -1)
+                    
+            blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        
+            mask = cv2.inRange(hsv, colorLower, colorUpper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+
+            edges = cv2.Canny(mask, 30, 150)
+            cnts = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+
+            if len(cnts) > 0:
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                if M["m00"] == 0 : M["m00"] = 1
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                
+                if ball_position[0] == -999 or ball_position[1] == -999 : 
+                    ball_position[0] = center[0]
+                    ball_position[1] = center[1] 
+                else:
+                    if previous_direction == '':
+                        previous_direction = utils.return_ball_direction_change(ball_position[1], center[1])
+                    else:
+                        current_direction = utils.return_ball_direction_change(ball_position[1], center[1])
+                        if previous_direction != current_direction:
+                            print('방향 바뀜')
+                    current_direction = previous_direction
+                    ball_position[0] = center[0]
+                    ball_position[1] = center[1]
+                    # 원 중심 좌표와 반지름을 이용하여 중심 계산
+                if radius > golfball_size:
+                    cv2.circle(frame, (int(x), int(y)), int(radius),
+                        (255, 0, 0), 2)
+                    cv2.circle(frame, center, 2, (255, 0, 0), -1)
+                    if x <= goal_y + 30:
+                        utils.goal(goal_y,y)
+
+                pts.appendleft(center)
+            cv2.imshow("Frame", frame)
+        else:
+            cv2.imshow('cap', cap)
+        
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    cv2.destroyAllWindows()
+
+def get_serial(conn):
+    myPort = serial.Serial('/dev/ttyUSB0', 9600,timeout=0.1)
+    myPort1 = serial.Serial('/dev/ttyUSB1', 9600, timeout=0.1)
+    time.sleep(0.5) 
+    myPort.reset_input_buffer()
+    myPort1.reset_input_buffer()
+    while True:
+        myString = myPort.readline().decode("latin-1").rstrip()
+        myString1 = myPort1.readline().decode("latin-1").rstrip()
+        if myString or myString1:
+            o1_bool, output = utils.is_valid_string(myString)
+            o2_bool, output1 = utils.is_valid_string(myString1)
+            if o1_bool or o2_bool:
+                conn.send([output, output1])
+        
+
+BALL_MOVEMENT_THRESHOLD = 10
+def check_movement(ball_pos):
+    while True:
+        initial_x = ball_pos[0]
+        initial_y = ball_pos[1]
+        print(f'initial value {initial_x} {initial_y}')
+        time.sleep(1)  # 2초 대기
+        current_x = ball_pos[0]
+        current_y = ball_pos[1]
+        print(f'current value {current_x} {current_y}')
+
+        if abs(current_x - initial_x) <= BALL_MOVEMENT_THRESHOLD and abs(current_y - initial_y) <= BALL_MOVEMENT_THRESHOLD:
+            print("The coordinates have not moved for 2 seconds.")
+        else:
+            print("The coordinates have moved.")
+
+if __name__ == '__main__':
+    with Manager() as manager:
+        parent_conn, child_conn = Pipe()
+        ball_position = manager.list()
+        shared_value = manager.Namespace()
+        shared_value.value = 0
+
+        ball_position.append(-999)
+        ball_position.append(-999)
+
+        p1 = Process(target=stream_opencv, args=(parent_conn, ball_position))
+        p2 = Process(target=get_serial, args=(child_conn,))
+        p3 = Process(target=check_movement,args=(ball_position,))
+        p4 = Process(target=tts_process, args=(shared_value, ))
+
+        p1.start()
+        p2.start()
+        p3.start()
+        p4.start()
+
+        p1.join()
+        p2.join()
+        p3.join()
+        p4.join()
