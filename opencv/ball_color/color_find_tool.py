4cb1994122d0dfffb74d301bc865a155448ffddc
gmseee <gmseee@naver.com>
2024-05-21 18:26:17 +0900

    Update color_find_tool.py

--- opencv/ball_color/color_find_tool.py
+++ opencv/ball_color/color_find_tool.py
@@ -2,6 +2,45 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2
 
+
+def camera_calibration(image):
+    # 카메라 보정 함수 
+    # image는 picam2.capture_array()로 받아온 frame
+    
+    # 최종 버전: v6
+    fx = 326.819
+    fy = 329.259
+    cx = 321.455
+    cy = 198.692
+    k1 = -0.298901
+    k2 = 0.072073
+    p1 = 0.001761
+    p2 = -0.000953
+
+    # 카메라 매트릭스와 왜곡 계수 정의
+    mtx = np.array([[fx, 0, cx],
+                    [0, fy, cy],
+                    [0, 0, 1]])
+    dist = np.array([[k1, k2, p1, p2]])
+
+    # 입력 이미지의 크기 가져오기
+    h, w = image.shape[:2]
+
+    # 최적의 새 카메라 매트릭스 계산
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    # 이미지를 새 카메라 매트릭스에 맞게 왜곡 보정
+    dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
+
+    # ROI(관심 영역) 추출
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+
+    return dst
+
+
+
+
 #picamera2 객체 생성 및 설정
 picam2 = Picamera2()
 picam2.video_configuration.controls.FrameRate = 60.0
@@ -17,6 +56,7 @@ upper_bgr = np.array([180, 255, 255])
 while True:
     #카메라에서 프레임 캡처
     frame = picam2.capture_array()
+    frame = camera_calibration(frame)
 
 
     # BGR이미지를 HSV로 전환
