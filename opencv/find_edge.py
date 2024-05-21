401bec4247d30e4c1c4da65146dbb414c15e1235
swanim <stellarswan20@gmail.com>
2024-05-21 13:25:31 +0100

    feat: add import utils

--- opencv/find_edge.py
+++ opencv/find_edge.py
@@ -4,6 +4,7 @@ from picamera2 import Picamera2
 import numpy as np
 import argparse
 import cv2
+import utils
 import imutils
 import time
 
@@ -34,19 +35,18 @@ while True:
     else:
         previous_frame = cap
 
-    cap = utils.camera_calibration(cap)
+    frame = utils.camera_calibration(cap)
     cv2.namedWindow('cap')
-    cv2.setMouseCallback('cap', get_position)
 
     # 프레임을 그레이스케일로 변환
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
      # 검은색의 HSV 범위 정의
-    lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 30])  # 이 범위는 필요에 따라 조정
+    lower_black = (0, 0, 0)
+    upper_black = (180, 255, 30)  # 이 범위는 필요에 따라 조정
 
     # 검은색 영역 마스크 생성
-    mask = cv2.inRange(hsv, lower_black, upper_black)
+    mask = cv2.inRange(gray, lower_black, upper_black)
 
 
     # 가우시안 블러 적용 (노이즈 제거)
