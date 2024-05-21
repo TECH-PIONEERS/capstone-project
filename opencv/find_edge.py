fe25602472465ad4c7f202329ab5604053af67d1
swanim <64563859+swanim@users.noreply.github.com>
2024-05-21 20:52:48 +0900

    검은색만 마스킹

--- opencv/find_edge.py
+++ opencv/find_edge.py
@@ -41,6 +41,14 @@ while True:
     # 프레임을 그레이스케일로 변환
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+     # 검은색의 HSV 범위 정의
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([180, 255, 30])  # 이 범위는 필요에 따라 조정
+
+    # 검은색 영역 마스크 생성
+    mask = cv2.inRange(hsv, lower_black, upper_black)
+
+
     # 가우시안 블러 적용 (노이즈 제거)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
