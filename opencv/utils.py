4e94a778a3c14c3f803f29db9e829ef4090a9b7c
YeJi-ing <ping_yeji@naver.com>
2024-06-05 16:19:20 +0900

    feat: temp_return_ball_direction 수정

--- opencv/utils.py
+++ opencv/utils.py
@@ -196,32 +196,34 @@ def is_valid_string(input_string):
     else: return False, arr
 
 
-def temp_return_ball_direction(previous_pos_x, previous_pos_y, current_pos_x, current_pos_y, previous_direction, threshold=5):
-    right_wall_y = 7 #7~15
-    left_wall_y  = 60 # 56~60
-    wall_threshold_y = 5
+def temp_return_ball_direction(previous_pos_x, previous_pos_y, current_pos_x, current_pos_y, previous_direction):
+    right_wall_y = 7 #7 ~ 15
+    left_wall_y  = 60 # 56 ~ 60
+    wall_detect_threshold_y = 7
+    wall_threshold_y = 2.5
+    threshold  = 5
 
     if is_within_goal(previous_pos_x, previous_pos_y) is True and is_within_goal(current_pos_x, current_pos_y) is True:
         #print("공이 홀 안에 있음")
         return previous_direction
 
-    if current_pos_y < right_wall_y + wall_threshold_y: # 공이 right 벽 근처에 있을 때 
-        #print("here is rw")
-        if abs(current_pos_y - previous_pos_y) >= threshold:
+    if current_pos_y < right_wall_y + wall_detect_threshold_y: # 공이 right 벽 근처에 있을 때 
+        if abs(current_pos_y - previous_pos_y) >= wall_threshold_y:
             return 'rw'
-    elif current_pos_y > left_wall_y - wall_threshold_y:# 공이 left 벽 근처에 있을 때 
-        #print("here is lw")
-        if abs(current_pos_y - previous_pos_y) >= threshold:
+    elif current_pos_y > left_wall_y - wall_detect_threshold_y:# 공이 left 벽 근처에 있을 때 
+        if abs(current_pos_y - previous_pos_y) >= wall_threshold_y:
             return 'lw'
-        return 'lw'
-    else: # 공의 방향
-        if abs(current_pos_y - previous_pos_y) >= threshold:
-            if previous_pos_y < current_pos_y:
-                return 'right'
-            elif previous_pos_y > current_pos_y:
-                return 'left'
-        else:
-            return 'straight'
+    else: 
+        return previous_direction
+    
+    # 그 외 범위 # 공의 방향
+    if abs(current_pos_y - previous_pos_y) >= threshold:
+        if previous_pos_y < current_pos_y:
+            return 'right'
+        elif previous_pos_y > current_pos_y:
+            return 'left'
+    else:
+        return 'straight'
 
 
 def return_ball_direction(previous_pos, current_pos, threshold=5):
